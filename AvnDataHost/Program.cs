a6e9bc7a2da15345d692cf09abd97d84f533f137
Trịnh Hữu Bảo Sơn <trinhhuubaoson.avn@gmail.com>
2018-10-06 16:48:50 +0700

    Create Data Service

---
+++ AvnDataHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace AvnDataHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost serviceHost = new ServiceHost(typeof(AvnDataService.Service1), new Uri("http://localhost:9009"));
+            serviceHost.AddServiceEndpoint(typeof(AvnDataService.IStaffService), new NetTcpBinding(), "net.tcp://localhost:9091");
+            serviceHost.Open();
+            Console.WriteLine("Service hosted;");
+            Console.ReadLine();
+        }
+    }
+}
--- AvnDataService/IService1.cs
+++ AvnDataService/IService1.cs
@@ -9,7 +9,7 @@ namespace AvnDataService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IStaffService
     {
         [OperationContract]
         string GetData(int value);
